a2debf3bad03c2abb31e69266647757f8da151be
Dmitry <dmytro.vaga@gmail.com>
2025-01-22 17:57:01 -0700

    remove modules

--- modules/ec2-public/output.tf
+++
@@ -1,9 +0,0 @@
-output "nginx_public_ip" {
-  value       = aws_instance.ec2_public.public_ip
-  description = "The public IP address of the ec2 instance."
-}
-
-output "nginx_eip" {
-  value       = aws_eip.ec2_public.public_ip
-  description = "EIP address of the ec2 instance."
-}
